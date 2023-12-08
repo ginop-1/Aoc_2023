f12db2352d8c7a3495012bf84e28abc91b7c5b0e
ginop-1 <henrycovili2004@gmail.com>
2023-12-08 10:44:56 +0100

    forgot to remove custom lcm func

--- 8.py
+++ 8.py
@@ -14,19 +14,6 @@ def getNodeIndex(target, nodes):
             return i
 
 
-def lcm(a, b):
-    if a > b:
-        greater = a
-    else:
-        greater = b
-    while True:
-        if (greater % a == 0) and (greater % b == 0):
-            lcm = greater
-            break
-        greater += 1
-    return lcm
-
-
 def p1(f):
     lines = f.read().splitlines()
     steps, nodes = lines[0], lines[2:]
